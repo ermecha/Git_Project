b20e766d5cbd253c3118325b57c5930535929623
ermek <sagynbekoff@icloud.com>
2022-03-26 14:42:53 -0500

    commit

--- src/GitExample.java
+++ src/GitExample.java
@@ -2,5 +2,6 @@ public class GitExample {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("some changes to share");
+        System.out.println("see ");
     }
 }
